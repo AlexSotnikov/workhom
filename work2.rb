11cdcab6b5d18fd9b5dd4ebf0ec2b28c514b5e18
AlexSotnikov <alex_sx_d@mail.ru>
2015-02-05 15:18:02 +0200

    1

--- work2.rb
+++ work2.rb
@@ -1,6 +1,10 @@
 def reduce(arr,ak)
   for e in(arr)
-     ak=yield ak,e
+     if block_given? 
+       ak=yield ak,e
+     else
+       ak
+     end
   end
   ak
 end
@@ -41,13 +45,17 @@ end
   end  
 #---------------------------
   def all?(arr,&block)
-     reduce arr,true do |ag,e| 
-       unless block.call(e) 
-        ag=false
-       else
-        ag
-       end
-     end 
+    unless arr==[]
+      reduce arr,true do |ag,e| 
+        unless block.call(e) 
+          ag=false
+        else
+          ag
+        end
+      end
+    else 
+      false
+    end 
   end 
 #----------------------------  
   def any?(arr,&block)
@@ -72,55 +80,61 @@ def flatten(arr)
   aend=true #переменная для завершения цикла 
   while aend #в цикле вызываем р до тех пор пока в ar есть подмассивы
     arr.each {|e| p.call(e)}
-    a=(any?(ar){|e| e.is_a?(Array)}) #проверка на подмассивы
+    aend=(any?(ar){|e| e.is_a?(Array)}) #проверка на подмассивы
     arr=ar
     if aend==true # обнуляем ar, если есть подмассивы
       ar=[]
     end 
   end
-ar
+  if ar==[]
+    nil
+  else
+    ar
+  end
 end
 #-------------------------- 
 def reverse(arr)
-  j=-1
-  ar=[]
-  for i in(0..(arr.size-1))
-    ar[i]=arr[j]
-    j=j-1  
+  if arr!=[]
+    j=-1
+    ar=[]
+    for i in(0..(arr.size-1))
+      ar[i]=arr[j]
+      j=j-1  
+    end
+    ar
   end
-ar
 end
 #--------------------------
 def sortarr(arr)
-  rez1=reduce(arr,[])do |acc,e|# част массива чет. чисел
-    if (e%2)==0 #проверка на чет.
-      acc << e
-    else
-      acc
+  if arr!=[]
+    rez1=reduce(arr,[])do |acc,e|# част массива чет. чисел
+      if (e%2)==0 #проверка на чет.
+        acc << e
+      else
+        acc
+      end
     end
-  end
-  rez2=reduce(arr,[])do |acc,e|# часть массива нечет. чисел
-    if (e%2)!=0 #проверка на нечет.
-      acc << e
-    else
-      acc
+    rez2=reduce(arr,[])do |acc,e|# часть массива нечет. чисел
+      if (e%2)!=0 #проверка на нечет.
+        acc << e
+      else
+        acc
+      end
     end
-  end
-  rez1=(rez1.sort.reverse)+(rez2.sort.reverse)
+    rez1=(rez1.sort.reverse)+(rez2.sort.reverse)
 #определяем новый массив из частей при этом сортируя и реверсируя их
+  end
 end
-p(sortarr([2,6,7,8,1,3]))
 #----------------------------------
 def localmax(arr)
   ar=[]
   arr.each_cons(3) do |e| # прсматриваем по 3 ел. делая проверку на макс.
-    if (e[1] > e[0])and(e[1] > e[2])
+    if (e[1] >= e[0])and(e[1] >= e[2])
       ar << e[1]
     end
   end
   ar
 end
-p localmax([1,5,3,1,4,2])
 #----------------------------------
 def merge(h1,h2)
   h2.each{|k,v| h1[k]=v} 
---
+++ work4.rb
@@ -0,0 +1,117 @@
+class Line
+  def initialize(x1,y1,x2,y2,f=1)
+    @x1=x1
+	@y1=y1
+	@x2=x2
+	@y2=y2
+	@f=f
+  end
+  def draft
+    s=('<line x1="')+(@x1.to_s)+('" y1="')+(@y1.to_s)+('" stroke="black')+
+	('" x2="')+(@x2.to_s)+('" y2="')+(@y2.to_s)+('" stroke-width="')+(@f.to_s)+('" />')
+  end
+end  
+class Rect
+  def initialize(x,y,w,h)
+    @x=x
+	@y=y
+	@w=w
+	@h=h
+  end
+  def draft
+    s=('<rect x="')+(@x.to_s)+('" y="')+(@y.to_s)+
+	('" width="')+(@w.to_s)+('" height="')+(@h.to_s)+('" />')
+  end
+end  
+class Circle
+  def initialize(x,y,r,f='none')
+    @x=x
+	@y=y
+	@r=r
+	@f=f
+  end
+  def draft
+    s=('<circle cx="')+(@x.to_s)+('" cy="')+(@y.to_s)+
+	('" r="')+(@r.to_s)+('" fill="')+(@f)+('" />')  
+  end
+end
+class Arrow < Line  
+  def draft
+    if @x2<@x1
+	  x=@x2+3
+	  y=@y1-@y2
+	else
+      x=@x2-3
+	  y=@y2-@y1
+	end
+    if @y1==@y2
+      s=('<line x1="')+(@x1.to_s)+('" y1="')+(@y1.to_s)+('" stroke="black')+
+	  ('" x2="')+(@x2.to_s)+('" y2="')+(@y2.to_s)+('" stroke-width="')+(@f.to_s)+('" /> ')+
+      ('<polygon points="')+(@x2.to_s)+(' ')+(@y2.to_s)+(', ')+(x.to_s)+(' ')+
+	  ((@y2-3).to_s)+(', ')+(x.to_s)+(' ')+((@y2+3).to_s)+('"/> ')
+	else
+	  s=('<line x1="')+(@x1.to_s)+('" y1="')+(@y1.to_s)+('" stroke="black')+
+	  ('" x2="')+(@x2.to_s)+('" y2="')+(@y2.to_s)+('" stroke-width="')+(@f.to_s)+('" /> ')+
+      ('<polygon points="')+(@x2.to_s)+(' ')+(@y2.to_s)+(', ')+(x.to_s)+(' ')+
+	  ((@y2-3).to_s)+(', ')+(x.to_s)+(' ')+((@y2+3).to_s)+('" transform="rotate(')+
+	  ((y*2.5).to_s)+(', ')+(@x2.to_s)+(', ')+(@y2.to_s)+(')" /> ')
+    end
+  end	
+end
+elements = [
+  Line.new(50, 160, 50, 220),
+  Rect.new(42, 120, 16, 40),
+  Line.new(75, 235, 100, 235),
+  Line.new(50, 275, 200, 275),
+  Circle.new(50, 180, 3, 'black'),
+  Line.new(50, 100, 50, 120),
+  Arrow.new(200, 250, 175, 240),
+  Line.new(75, 220, 75, 250, 3),
+  Circle.new(100, 100, 3, 'black'),
+  Line.new(100, 160, 100, 180),
+  Line.new(200, 220, 175, 230),
+  Line.new(110, 290, 140, 290),
+  Line.new(178, 165, 178, 195),
+  Line.new(200, 160, 200, 220),
+  Line.new(100, 180, 150, 235),
+  Line.new(150, 180, 100, 235),
+  Line.new(200, 250, 200, 275),
+  Line.new(200, 100, 200, 120),
+  Circle.new(125, 275, 3, 'black'),
+  Line.new(78, 165, 78, 195),
+  Arrow.new(50, 100, 250, 100),
+  Circle.new(100, 180, 3, 'black'),
+  Line.new(100, 100, 100, 120),
+  Rect.new(92, 120, 16, 40),
+  Circle.new(200, 100, 3, 'black'),
+  Circle.new(200, 180, 3, 'black'),
+  Line.new(50, 180, 72, 180),
+  Line.new(72, 165, 72, 195),
+  Circle.new(70, 235, 25),
+  Line.new(150, 100, 150, 120),
+  Line.new(125, 275, 125, 290),
+  Line.new(78, 180, 100, 180),
+  Line.new(150, 180, 172, 180),
+  Circle.new(180, 235, 25),
+  Line.new(172, 165, 172, 195),
+  Circle.new(150, 180, 3, 'black'),
+  Arrow.new(178, 180, 250, 180),
+  Line.new(150, 160, 150, 180),
+  Line.new(175, 235, 150, 235),
+  Rect.new(142, 120, 16, 40),
+  Line.new(50, 250, 50, 275),
+  Circle.new(150, 100, 3, 'black'),
+  Rect.new(192, 120, 16, 40),
+  Line.new(50, 220, 75, 230),
+  Line.new(175, 220, 175, 250, 3),
+  Arrow.new(50, 250, 75, 240)
+]
+File.open('image.svg','wb') do |f|
+  f.puts('<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="300">')
+  elements.each do |el|
+    f.puts(el.draft)
+  end	 
+     
+   f.puts('</svg>')
+ end  
